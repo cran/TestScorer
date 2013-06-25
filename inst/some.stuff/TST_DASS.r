a197ed9072c55b7469253dbdfa54a3db8bafaaf4
Manel Salamero <manelsalamero@gmail.com>
2013-06-25 01:22:13 +0000

    version 1.2

--- inst/some.stuff/TST_DASS.r
+++ inst/some.stuff/TST_DASS.r
@@ -1,20 +1,18 @@
-# DASS scale scoring commands
-# Creation date: 2013-04-02
+# DASS scale scoring script
+# Creation date: 2013-06-20
 # --------------
 
-testChar <- list(
-  acronym="DASS",
-  name="Depression, Anxiety & Stress Scales",
-  ref="Lovibond & Lovibond, 1995",
-  n.items=42,
-  valid=c(0, 1, 2, 3),
-  miss=c(4),
-  comm="Public domain: ww2.psy.unsw.edu/dass/"
-) # end testChar
+testChar <- list(acronym = "DASS",
+                 name = "Depression Anxiety Stress Scales",
+                 ref = "SH Lovibond & PF Lovibond, 1995",
+                 n.items = 42,
+                 valid = c(0, 1, 2, 3),
+                 miss = c(4),
+                 comm = "Public domain: www2.psy.unsw.edu.au/dass")
 
-scoring.fun <- function(answers, sex, age=0, id, date.test, comm)
-# "answer" is a *character* vector as introduced through the keyboard.
-{
+scoring.fun <- function(answers, sex, age, id, date.test, comm) {
+  # "answer" is a *character* vector as introduced through the keyboard.
+  # "sex", "age", "id", "date.test" & "comm" used if appropiate.
   answers <- as.numeric(answers) # transform to numeric for easier scoring
   answers[answers %in% c(4)] <- NA # missing characters to NA
   blanks <- sum(is.na(answers)) # compute number of missings
@@ -23,48 +21,83 @@ scoring.fun <- function(answers, sex, age=0, id, date.test, comm)
 
   # D scale scoring commands
   # --------------
-  results[2, "Acronym"] <- "D" # acronym
-  results[2, "Scale"] <- "Depression" # name of the scale
-  items <- c(3, 5, 10, 13, 16, 17, 21, 24, 26, 31, 34, 37, 38, 42) # items making up the scale
-  results[2, "Miss"] <- sum(is.na(answers[items])) # number of missings
-  results[2, "Raw"] <- sum(answers[items], na.rm=TRUE) # raw score (sum answered items)
-  trans.table <- c("5-30", "35-45", "50-55", "60-65", "70-75", "75-79", "80-83", "84-86", "87-89", "90", "91", "92", "93", "94", "94", "95", "95", "96", "96", "96", "97", "97", "98", "98", "98", "98", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99")
-  results[2, "Centil"] <- trans.table[round(results[2, "Raw"]) + 1] # + 1, because trans begins at raw score 0
+  results[1, "Acronym"] <- "D" # acronym
+  results[1, "Scale"] <- "Depression" # name of the scale
+  # Items making up the scale
+  items <- c(3, 5, 10, 13, 16, 17, 21, 24, 26, 31, 34, 37, 38, 42)
+  results[1, "Miss"] <- sum(is.na(answers[items])) # number of missings
+  if (results[1, "Miss"] == length(items)) {  # all answers are missings
+    results[1, "Raw"] <- NA
+    results[1, "Centil"] <- NA
+  } else {
+    results[1, "Raw"] <- sum(answers[items], na.rm=TRUE) # sum answered items
+  # Vector for score transformation
+    trans.table <- c("5-20", "25-35", "40-45", "50-55", "60", "65", "70", "75", "76-78", "79-81", "82-83", "84-85", "86", "87", "88-89", "90", "91", "92", "93", "93", "94", "94", "95", "95", "96", "96", "96", "97", "97", "97", "97", "98", "98", "98", "98", "98", "99", "99", "99", "99", "99", "99", "99")
+    index <- results[1, "Raw"]
+    index <- 1 + index  # 1, because raw scores begin at 0
+    results[1, "Centil"] <- trans.table[index]
+  }
 
   # A scale scoring commands
   # --------------
-  results[1, "Acronym"] <- "A" # acronym
-  results[1, "Scale"] <- "Anxiety" # name of the scale
-  items <- c(2, 4, 7, 9, 15, 19, 20, 23, 25, 28, 30, 36, 40, 41) # items making up the scale
-  results[1, "Miss"] <- sum(is.na(answers[items])) # number of missings
-  results[1, "Raw"] <- sum(answers[items], na.rm=TRUE) # raw score (sum answered items)
-  trans.table <- c("5-20", "25-35", "40-45", "50-55", "60", "65", "70", "75", "79-78", "79-81", "82-83", "84-85", "86", "87", "88-89", "90", "91", "92", "93", "93", "94", "94", "95", "95", "96", "96", "96", "97", "97", "97", "87", "98", "98", "98", "98", "98", "99", "99", "99", "99", "99", "99", "99")
-  results[1, "Centil"] <- trans.table[round(results[1, "Raw"]) + 1] # + 1, because trans begins at raw score 0
+  results[2, "Acronym"] <- "A" # acronym
+  results[2, "Scale"] <- "Anxiety" # name of the scale
+  # Items making up the scale
+  items <- c(2, 4, 7, 9, 15, 19, 20, 23, 25, 28, 30, 36, 40, 41)
+  results[2, "Miss"] <- sum(is.na(answers[items])) # number of missings
+  if (results[2, "Miss"] == length(items)) {  # all answers are missings
+    results[2, "Raw"] <- NA
+    results[2, "Centil"] <- NA
+  } else {
+    results[2, "Raw"] <- sum(answers[items], na.rm=TRUE) # sum answered items
+  # Vector for score transformation
+    trans.table <- c("5-30", "35-45", "50-55", "60-65", "70-75", "76-79", "80-83", "84-86", "87-89", "90", "91", "92", "93", "94", "94", "95", "95", "96", "96", "96", "97", "97", "98", "98", "98", "98", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99", "99")
+    index <- results[2, "Raw"]
+    index <- 1 + index  # 1, because raw scores begin at 0
+    results[2, "Centil"] <- trans.table[index]
+  }
 
   # S scale scoring commands
   # --------------
   results[3, "Acronym"] <- "S" # acronym
   results[3, "Scale"] <- "Stress" # name of the scale
-  items <- c(1, 6, 8, 11, 12, 14, 18, 22, 27, 29, 32, 33, 35, 39) # items making up the scale
+  # Items making up the scale
+  items <- c(1, 6, 8, 11, 12, 14, 18, 22, 27, 29, 32, 33, 35, 39)
   results[3, "Miss"] <- sum(is.na(answers[items])) # number of missings
-  results[3, "Raw"] <- sum(answers[items], na.rm=TRUE) # raw score (sum answered items)
-  trans.table <- c("5", "10", "15", "20-25", "30", "35", "40", "45", "50-55", "60", "65", "65", "70", "75-77", "78-80", "81-83", "83-84", "85-86", "87-88", "89", "90", "91", "92", "93", "93", "94", "95", "95", "96", "96", "97", "97", "97", "97", "98", "98", "98", "99", "99", "99", "99", "99", "99")
-  results[3, "Centil"] <- trans.table[round(results[3, "Raw"]) + 1] # + 1, because trans begins at raw score 0
+  if (results[3, "Miss"] == length(items)) {  # all answers are missings
+    results[3, "Raw"] <- NA
+    results[3, "Centil"] <- NA
+  } else {
+    results[3, "Raw"] <- sum(answers[items], na.rm=TRUE) # sum answered items
+  # Vector for score transformation
+    trans.table <- c("5", "10", "15", "20-25", "30", "35", "40", "45", "50-55", "60", "65", "65", "70", "75-77", "78-80", "81-82", "83-84", "85-86", "87-88", "89", "90", "91", "92", "93", "93", "94", "95", "95", "96", "96", "97", "97", "97", "97", "98", "98", "98", "99", "99", "99", "99", "99", "99")
+    index <- results[3, "Raw"]
+    index <- 1 + index  # 1, because raw scores begin at 0
+    results[3, "Centil"] <- trans.table[index]
+  }
 
-  # One row data frame with scores, for writing scores into a data file
+  # Vector for writing scores to a file
   # --------------------
-  results.scores <- data.frame(t(c(blanks, results[, "Raw"], results[,"Centil"])))
-  names(results.scores) <- c("blanks", paste(results[["Acronym"]], "raw", sep="_"), paste(results[["Acronym"]], "C", sep="_"))
+  results.scores <- unlist(results[-c(1, 2)])
+  names <- paste(results$Acronym, names(results.scores), sep=".")
+  names(results.scores) <- sub("[0-9]+$", "", names)
 
   # Output in form of list
   # ------------------
-  results.lst <- list('Centils calculated from 1.771 members of both sexes',
-                      'of the UK general adult population. Data from Crawford JR,',
-                      'Crawford JD. Br J Clin Psuchol, 2003, 42, 111:131.\n',
-                      paste("Total number of missings: ", blanks, " (", pcnt.blanks, "%)", sep=""))
+  results.lst <- list(paste("Total number of missings: ",
+                            blanks,
+                            " (",
+                            pcnt.blanks,
+                            "%)",
+                            sep=""),
+                       paste("\nNormative data from a non-clinical British sample. Data from",
+                             "JR Crawford. & JD Henry, Br J Clin Psychol 2003, 42:111-131.",
+                             sep='\n'))
 
   # Return results
   # ------------------
-  return(list(results.lst=results.lst, results.df=results, results.scores=results.scores))
+  return(list(results.lst = results.lst,
+              results.df = results,
+              results.scores = results.scores))
 
 } # end of scoring.fun
--- inst/some.stuff/TST_RAAS.r
+++ inst/some.stuff/TST_RAAS.r
@@ -1,97 +1,121 @@
-# RAAS scale scoring commands
-# Creation date: 2013-04-02
+# RAAS scale scoring script
+# Creation date: 2013-05-13
 # --------------
 
-testChar <- list(
-  acronym="RAAS",
-  name="Revised Adult Attachment Scale",
-  ref="Collins, 1996",
-  n.items=18,
-  valid=c(1, 2, 3, 4, 5),
-  miss=c(0),
-  comm="Public domain: www.openpsychassessment.org/wp-content/uploads/2011/06/AdultAttachmentScale.pdf"
-) # end testChar
+testChar <- list(acronym = "RAAS",
+                 name = "Revised Adult Attachment Scale",
+                 ref = "N. Collins, 1996",
+                 n.items = 18,
+                 valid = c(1, 2, 3, 4, 5),
+                 miss = c(0),
+                 comm = "Public domain: www.openpsychassessment.org/wp-content/uploads/2011/06/AdultAttachmentScale.pdf")
 
-scoring.fun <- function(answers, sex, age=0, id, date.test, comm)
-# "answer" is a *character* vector as introduced through the keyboard.
-{
+scoring.fun <- function(answers, sex, age, id, date.test, comm) {
+  # "answer" is a *character* vector as introduced through the keyboard.
+  # "sex", "age", "id", "date.test" & "comm" used if appropiate.
   answers <- as.numeric(answers) # transform to numeric for easier scoring
   answers[answers %in% c(0)] <- NA # missing characters to NA
   blanks <- sum(is.na(answers)) # compute number of missings
   pcnt.blanks <- round((blanks / 18) * 100) # compute % of missings
+  # Items which should be reversed
   reversed.items=c(2, 7, 8, 13, 16, 17, 18)
-  answers[reversed.items] <- (5 + 1) - answers[reversed.items] # reverse items
+  answers[reversed.items] <- (5 + 1) - answers[reversed.items]
   results <- data.frame(NULL) # Null data frame for results
 
-  toT <- function(raw.score, mean, sd) # compute T score
-  {
+  toT <- function(raw.score, mean, sd) {  # compute T score
     T.score <- round(((raw.score - mean) / sd) * 10 + 50)
     return(T.score)
   } # end toT
 
-  makeGraph <- function(T.score) # make graph
-  {
-    if (!is.na(T.score)) # avoids error
-    {
-      graph  <- "|    :    :    |    :    |    :    |    :    :    |"
+  makeGraph <- function(T.score) {  # make a graph
+    template  <- "|    :    :    |    :    |    :    |    :    :    |"
+    options(warn=-1)
+    T.score <- as.integer(T.score)  # in case of numerical string
+    options(warn=0)
+    if (!is.na(T.score)) {
       if (T.score < 0) T.score <- 0
         else if (T.score > 100) T.score <- 100
-      position <- round((T.score/2)+1)
-      graph <- paste(substr(graph, 1, position-1), substr(graph, position + 1, nchar(graph)), sep="o")
+      position <- round((T.score / 2) + 1)
+      graph <- paste(substr(template, 1, position-1),
+                     substr(template, position + 1, nchar(template)),
+                     sep="o")
+    } else {
+      graph <- "Not graphicable"
     }
-    else graph <- NA
+    return(graph)
   } # end makeGraph
 
   # C scale scoring commands
   # --------------
   results[1, "Acronym"] <- "C" # acronym
   results[1, "Scale"] <- "Close" # name of the scale
-  items <- c(1, 6, 8, 12, 13, 17) # items making up the scale
+  # Items making up the scale
+  items <- c(1, 6, 8, 12, 13, 17)
   results[1, "Miss"] <- sum(is.na(answers[items])) # number of missings
-  results[1, "Raw"] <- round(mean(answers[items], na.rm=TRUE), 2) # raw score (mean answered items
-  if (sex=="male") results[1, "T"] <- toT(results[1, "Raw"], 21.54, 5.14) # compute T score
-    else results[1, "T"] <- toT(results[1, "Raw"], 21.92, 5.19)
-  results[1, "Graph"] <- makeGraph(results[1, "T"]) # make the graph
+  if (results[1, "Miss"] == length(items)) {  # all answers are missings
+    results[1, "Raw"] <- NA
+    results[1, "T"] <- NA
+    results[1, "Graph"] <- "All missings"
+  } else {
+    results[1, "Raw"] <- round(mean(answers[items], na.rm=TRUE), 2) # mean answered items
+    if (sex=="Male") results[1, "T"] <- toT(results[1, "Raw"], 21.54, 5.14) # compute T score
+      else results[1, "T"] <- toT(results[1, "Raw"], 21.92, 5.19)
+    results[1, "Graph"] <- makeGraph(results[1, "T"]) # make the graph
+  }
 
   # D scale scoring commands
   # --------------
   results[2, "Acronym"] <- "D" # acronym
   results[2, "Scale"] <- "Dependent" # name of the scale
-  items <- c(2, 5, 7, 14, 16, 18) # items making up the scale
+  # Items making up the scale
+  items <- c(2, 5, 7, 14, 16, 18)
   results[2, "Miss"] <- sum(is.na(answers[items])) # number of missings
-  results[2, "Raw"] <- round(mean(answers[items], na.rm=TRUE), 2) # raw score (mean answered items
-  if (sex=="male") results[2, "T"] <- toT(results[2, "Raw"], 20.59, 5) # compute T score
-    else results[2, "T"] <- toT(results[2, "Raw"], 19.51, 5.14)
-  results[2, "Graph"] <- makeGraph(results[2, "T"]) # make the graph
+  if (results[2, "Miss"] == length(items)) {  # all answers are missings
+    results[2, "Raw"] <- NA
+    results[2, "T"] <- NA
+    results[2, "Graph"] <- "All missings"
+  } else {
+    results[2, "Raw"] <- round(mean(answers[items], na.rm=TRUE), 2) # mean answered items
+    if (sex=="Male") results[2, "T"] <- toT(results[2, "Raw"], 20.59, 5) # compute T score
+      else results[2, "T"] <- toT(results[2, "Raw"], 19.51, 5.14)
+    results[2, "Graph"] <- makeGraph(results[2, "T"]) # make the graph
+  }
 
   # A scale scoring commands
   # --------------
   results[3, "Acronym"] <- "A" # acronym
   results[3, "Scale"] <- "Anxiety" # name of the scale
-  items <- c(3, 4, 9, 10, 11, 15) # items making up the scale
+  # Items making up the scale
+  items <- c(3, 4, 9, 10, 11, 15)
   results[3, "Miss"] <- sum(is.na(answers[items])) # number of missings
-  results[3, "Raw"] <- round(mean(answers[items], na.rm=TRUE), 2) # raw score (mean answered items
-  if (sex=="male") results[3, "T"] <- toT(results[3, "Raw"], 13.86, 5.36) # compute T score
-    else results[3, "T"] <- toT(results[3, "Raw"], 15.7, 6.05)
-  results[3, "Graph"] <- makeGraph(results[3, "T"]) # make the graph
+  if (results[3, "Miss"] == length(items)) {  # all answers are missings
+    results[3, "Raw"] <- NA
+    results[3, "T"] <- NA
+    results[3, "Graph"] <- "All missings"
+  } else {
+    results[3, "Raw"] <- round(mean(answers[items], na.rm=TRUE), 2) # mean answered items
+    if (sex=="Male") results[3, "T"] <- toT(results[3, "Raw"], 13.86, 5.36) # compute T score
+      else results[3, "T"] <- toT(results[3, "Raw"], 15.7, 6.05)
+    results[3, "Graph"] <- makeGraph(results[3, "T"]) # make the graph
+  }
 
-  # One row data frame with scores, for writing scores into a data file
+  # Vector for writing scores to a file
   # --------------------
-  results.scores <- data.frame(t(c(blanks, results[, "Raw"], results[,"T"])))
-  names(results.scores) <- c("blanks", paste(results[["Acronym"]], "raw", sep="_"), paste(results[["Acronym"]], "T", sep="_"))
+  results.scores <- unlist(results[-c(1, 2, 6)])
+  names <- paste(results$Acronym, names(results.scores), sep=".")
+  names(results.scores) <- sub("[0-9]+$", "", names)
 
-  # Ruler for graph column name
+  # Ruler for graph column
   # --------------------
   names(results)[6] <- "0    10   20   30   40   50   60   70   80   90  100"
 
   # ===== ADDITIONAL CODE INSERTED MANUALLY
   # Combine C & D scales
   CD <- round(mean(c(results[1, 'Raw'], results[2, 'Raw'])), 2)
-  results[4, ] <- c('', '','','','','') # blank row to improve readability
-  results[5, ] <- c('CD', 'Close/Dependent', '', CD, '', '') # no data for computing T score
+  results[4, ] <- c("", "", "", "", "", "") # blank row to improve readability
+  results[5, ] <- c("CD", "Close/Dependent", "", CD, "", "") # no data for T score
   
   # Attachment style assignement
-  style <- 'Located at midline'
   if (is.na(CD)) style <- 'Not evaluable' # if all answers are missings
   else if (CD > 3 & results[3, 'Raw'] < 3) style <- 'Secure'
   else if (CD > 3 & results[3, 'Raw'] > 3) style <- 'Preoccupied'
@@ -103,7 +127,7 @@ scoring.fun <- function(answers, sex, age=0, id, date.test, comm)
   windows(title="Attachment style")
   plot(CD, results[3, 'Raw'], xlim=c(1,5), ylim=c(1,5), pch=3, cex=2, col='blue', lwd=5,
        xlab='Close/Dependent', ylab='Anxiety', main=paste(id, date.test),
-       font.sub=2, sub='Position of the subject is represented by a blue cross')
+       font.sub=2, sub='The position of the subject is represented by a blue cross')
   abline(v=3)
   abline(h=3)
   text(2, 2, labels='Dismissing', col='gray60', font=2, cex=2)
@@ -111,28 +135,31 @@ scoring.fun <- function(answers, sex, age=0, id, date.test, comm)
   text(2, 4, labels='Fearful', col='gray60', font=2, cex=2)
   text(4, 4, labels='Preocupied', col='gray60', font=2, cex=2)
   # ===== END ADDITIONAL CODE INSERTED MANUALLY
-
+  
   # Output in form of list
   # ------------------
-  results.lst <- list(paste("Total number of missings: ", blanks, " (", pcnt.blanks, "%)", sep=""),
-            # ===== ADDITIONAL CODE INSERTED MANUALLY
-                      "",
-                      "RAAS Collis, 1996. Public domain document downloadable from:",
-                      "http://www.openpsychassessment.org/wp-content/uploads/2011/06/",
-                      "AdultAttachmentScale.pdf, downloaded: 29 sept 2012.",
-                      "",
-                      "According to the author, attach styles assignement 'is quite exploratory...",
-                      "[use] with caution, and only in conjunction with the continuous measures.'",
-                      "",
-                      paste("Attach style:", style),
-                      "",
-                      "T scores computed using mean and standard deviation from 414 USA college students,",
-                      "reported by Ledley et al. J Psychopath Behav Assess 2006, 28:33-40."
-            # ===== END ADDITIONAL CODE INSERTED MANUALLY
-			          )
-  
+  results.lst <- list(paste("Total number of missings: ",
+                            blanks,
+                            " (",
+                            pcnt.blanks,
+                            "%)",
+                            sep=""),
+                            # ===== ADDITIONAL CODE INSERTED MANUALLY
+                            "",
+                            "According to the author, attach styles assignement 'is quite exploratory...",
+                            "[use] with caution, and only in conjunction with the continuous measures.'",
+                            "",
+                            paste("Attach style:", style),
+                            "",
+                            "T scores computed using mean and standard deviation from 414 USA college students,",
+                            "reported by Ledley et al. J Psychopath Behav Assess 2006, 28:33-40."
+                            # ===== END ADDITIONAL CODE INSERTED MANUALLY
+                      )                            
+
   # Return results
   # ------------------
-  return(list(results.lst=results.lst, results.df=results, results.scores=results.scores))
+  return(list(results.lst = results.lst,
+              results.df = results,
+              results.scores = results.scores))
 
 } # end of scoring.fun
